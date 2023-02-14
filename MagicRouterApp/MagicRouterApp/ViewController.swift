2ba1b5491f86a61eded2f2fa0c8f0fde322a56b7
cellgit <liuhongli2120@gmail.com>
2023-02-14 14:16:07 +0800

    [t0]create MagicRouter main project

---
+++ MagicRouterApp/MagicRouterApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MagicRouterApp
+//
+//  Created by liuhongli on 2023/2/14.
+//
+
+import UIKit
+import MagicRouter
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+    }
+
+
+}
+
